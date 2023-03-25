78400614bc250617d62a9174abda5cfa4dff29f3
Azizjon Aliev <azizaliev2337@gmail.com>
2023-03-25 05:00:32 +0500

    adsnksd

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,8 +1,6 @@
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from attendance import views
-from core import settings
 
 urlpatterns = [
     path('attendances/export/excel/', views.export_attendances_xls, name='export_excel'),
@@ -10,6 +8,4 @@ urlpatterns = [
     path("attendances/", views.get_attendance, name="attendance"),
     path("attendances/add", views.create_attendance, name="attendance-create"),
     path("attendances/edit", views.update_attendance, name="attendance-update"),
-    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from core import settings
+from django.conf.urls.static import static
 from attendance.views import export_attendances_xls
 
 urlpatterns = [
     path('api/v1/', include('attendance.urls')),
     path('', admin.site.urls),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
